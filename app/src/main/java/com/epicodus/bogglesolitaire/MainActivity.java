e1817b7745e1cff8a6bc35a87bbea5cd123a9070
Trevor Gill <trevor.a.gill@gmail.com>
2017-10-11 09:33:04 -0700

    Initial commit

---
+++ app/src/main/java/com/epicodus/bogglesolitaire/MainActivity.java
@@ -0,0 +1,19 @@
+package com.epicodus.bogglesolitaire;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.puzzleString) TextView mPuzzleString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+}
